cea3005cc46075dd9d2bad4eece725b38b527ef8
unknown <rakhosch@gmail.comname>
2025-05-01 23:37:48 +0200

    update

--- routers/artikel_routes.py
+++ routers/artikel_routes.py
@@ -18,8 +18,19 @@ async def add_artikel(new_artikel: Artikel):
     await database.execute(query)
     return new_artikel
 
-# Artikel abrufen
+# Alle Artikel abrufen
 @router.get("/artikel", response_model=List[Artikel])
 async def artikel_abruf():
     query = artikel.select()
     return await database.fetch_all(query)
+
+# Einzelne Artikel abrufen
+@router.get("/artikel/{id}", response_model=Artikel)
+async def einzelner_artikel_abruf(abruf_id: int):
+    query = artikel.select().where(artikel.c.id == abruf_id)
+    abgerufener_artikel = database.fetch_one(query)
+
+    if not abgerufener_artikel:
+        raise HTTPException(status_code=404, detail="Kein Artikel mit der Such-ID gefunden")
+    else:
+        return abgerufener_artikel
